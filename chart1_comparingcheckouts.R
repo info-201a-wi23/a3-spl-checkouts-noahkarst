9ae84cac758143ea68a1eda64387e3baf7f7a798
noahkarst <noahekarst@gmail.com>
2023-02-26 19:30:38 -0800

    Completed Summary and Graphs

--- chart1_comparingcheckouts.R
+++ chart1_comparingcheckouts.R
@@ -12,6 +12,7 @@ spl_df1 <- df %>% filter(CheckoutYear < 2023)
 
 ####
 
+# Filtering for BOOK, EBOOK, and AUDIOBOOK checkouts per year
 materialtype_trend <- spl_df1 %>% filter(MaterialType %in% c("BOOK", "EBOOK", "AUDIOBOOK")) %>% group_by(CheckoutYear, MaterialType) %>% summarize(total_checkouts = sum(Checkouts))
 
 # Plotting the comparison
--- summary.R
+++ summary.R
@@ -12,22 +12,19 @@ spl_df1 <- df %>% filter(CheckoutYear < 2023)
 
 ####
 
-# BOOK checkouts per year
-book_checkouts_yr <- spl_df1 %>% filter(MaterialType == "BOOK") %>% group_by(CheckoutYear) %>% summarize(total_checkouts = sum(Checkouts))
+# Filtering for BOOK, EBOOK, and AUDIOBOOK checkouts per year
+materialtype_trend <- spl_df1 %>% filter(MaterialType %in% c("BOOK", "EBOOK", "AUDIOBOOK")) %>% group_by(CheckoutYear, MaterialType) %>% summarize(total_checkouts = sum(Checkouts))
 
-# EBOOK checkouts per year
-ebook_checkouts_yr <- spl_df1 %>% filter(MaterialType == "EBOOK") %>% group_by(CheckoutYear) %>% summarize(total_checkouts = sum(Checkouts))
-
-# AUDIOBOOK checkouts per year
-audiobook_checkouts_yr <- spl_df1 %>% filter(MaterialType == "AUDIOBOOK") %>% group_by(CheckoutYear) %>% summarize(total_checkouts = sum(Checkouts))
-
-total_book_checkouts_2020 <- book_checkouts_yr %>% filter(CheckoutYear == 2020)%>% pull(total_checkouts)
+# Book checkouts in 2020
+total_book_checkouts_2020 <- materialtype_trend %>% filter(CheckoutYear == 2020 & MaterialType == "BOOK")%>% pull(total_checkouts)
 # 609671
 
-total_ebook_checkouts_2020 <- ebook_checkouts_yr %>% filter(CheckoutYear == 2020)%>% pull(total_checkouts)
+# eBook checkouts in 2020
+total_ebook_checkouts_2020 <- materialtype_trend %>% filter(CheckoutYear == 2020 & MaterialType == "EBOOK")%>% pull(total_checkouts)
 # 1629338
 
-total_audiobook_checkouts_2020 <- audiobook_checkouts_yr %>% filter(CheckoutYear == 2020)%>% pull(total_checkouts)
+# Audiobook checkouts in 2020
+total_audiobook_checkouts_2020 <- materialtype_trend %>% filter(CheckoutYear == 2020 & MaterialType == "AUDIOBOOK")%>% pull(total_checkouts)
 # 1040745
 
 ####
@@ -39,14 +36,14 @@ df <- read.csv("~/Downloads/2013-2023-5-Checkouts-SPL.csv", stringsAsFactors = F
 spl_df2 <- df %>% filter(CheckoutYear == 2020)
 
 # Filtering for the 10 most popular book subjects in 2020
-subjects_checkouts <- spl_df2 %>% group_by(subjects = sub(", .*", "", Subjects)) %>% summarize(total_checkouts = sum((Checkouts)))
-top10subjects <- subjects_checkouts %>% arrange(desc(total_checkouts)) %>% slice(1:10)
+top10subjects <- spl_df2 %>% group_by(subjects = sub(", .*", "", Subjects)) %>%
+  summarize(total_checkouts = sum((Checkouts))) %>%
+  arrange(desc(total_checkouts)) %>% slice(1:10)
 
 ####
 
-# Filtering for the top 10 Creators in 2020
-creator_checkouts <- spl_df2 %>% group_by(Creator) %>% summarize(total_checkouts = sum((Checkouts))) %>% na_if("") %>% na.omit
-top10creators <- creator_checkouts %>% arrange(desc(total_checkouts)) %>% slice(1:10)
+# Filtering for top 3 creators each month of 2020
+top3creators_2020 <- spl_df2 %>% group_by(CheckoutMonth, Creator) %>% summarize(total_checkouts = sum(Checkouts)) %>% na_if("") %>% na.omit %>% arrange(desc(total_checkouts)) %>% slice(1:3)
 
 ####
 
